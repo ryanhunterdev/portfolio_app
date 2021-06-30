c6e06f3bb476682a14d7a1def0b5e182b5ea4b4d
Sophie Townsend <se.townsend31@gmail.com>
2021-06-30 14:40:51 +1000

    server js file updates

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
-console.log("first try");
+const express = require('express');
+const app = express();
+const port = 8080;
+
+const logger = require('./middlewares/logger.js');
+const apiController = require()
+const pagesController = require()
+
+app.listen(port, ()=>{
+    console.log(`listening on port ${port}`);
+})
+
+app.use(logger)
+
+app.use(express.static('client'))
+
+app.use(express.json())
+
+app.use('', apiController)
+
+// app.use('', pagesController)
