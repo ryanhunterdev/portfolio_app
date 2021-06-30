1b9edf3deb34e0c4cf27fb812a5a8df031dfe011
Sophie Townsend <se.townsend31@gmail.com>
2021-06-30 15:21:05 +1000

    working on server

--- server.js
+++ server.js
@@ -3,19 +3,26 @@ const app = express();
 const port = 8080;
 
 const logger = require('./middlewares/logger.js');
-const apiController = require()
-const pagesController = require()
+// const apiController = require()
+// const pagesController = require()
+
+app.set('view engine', 'ejs')
+app.set('views', './templates')
 
 app.listen(port, ()=>{
     console.log(`listening on port ${port}`);
 })
 
+app.get('/', (req, res) => {
+    res.render('')
+})
+
 app.use(logger)
 
-app.use(express.static('client'))
+// app.use(express.static('client'))
 
 app.use(express.json())
 
-app.use('', apiController)
+// app.use('', apiController)
 
 // app.use('', pagesController)
