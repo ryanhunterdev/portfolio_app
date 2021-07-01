b85a7c3b339019e58fe6104694e7d684fb94b323
Sophie Townsend <se.townsend31@gmail.com>
2021-07-01 18:40:28 +1000

    editing skills

--- controllers/new-portfolio-controller.js
+++ controllers/new-portfolio-controller.js
@@ -24,7 +24,7 @@ router.patch('/api/portfolios/add/skills/:id', (req, res) =>{
     .then(dbRes => {
       res.status(201).json({
         message: "skills set was successfully added",
-        skill: dbRes.rows
+        skill: dbRes.rows[0].skills[dbRes.rows[0].skills.length-1]
       })
     })
 })
