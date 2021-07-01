18ae818ee40198df99a8081674ac3f4415054011
Ryan Hunter <ryanhunterdev@gmail.com>
2021-07-01 12:18:09 +1000

    working on the finished portfolio ejs and the corresponding js file

---
+++ client/js/finished_portfolio.js
@@ -0,0 +1,23 @@
+// page variables
+
+const userName = document.querySelector('.user-name');
+const userLocation = document.querySelector('.user-location');
+const userWelcome = document.querySelector('.user-welcome');
+const contactPitch = document.querySelector('.contact-pitch');
+const emailAnchor = document.querySelector('.email-anchor');
+const githubAnchor = document.querySelector('.github-anchor');
+const copyrightName = document.querySelector('.copyright-name');
+const linkedin = document.querySelector('.linkedin');
+const instagram = document.querySelector('.instagram');
+const twitter = document.querySelector('.twitter');
+const projectsGrid = document.querySelector('.projects-grid');
+const skillsGrid = document.querySelector('.skills-grid');
+
+
+function populatePage() {
+    console.log('working')
+}
+
+
+
+window.addEventListener('load', populatePage)
