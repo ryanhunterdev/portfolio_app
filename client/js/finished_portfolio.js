6d5ffa396ce95a9cf6fd90342b2b9d683bee1f9f
Ryan Hunter <ryanhunterdev@gmail.com>
2021-07-01 14:04:24 +1000

    got javascript working to populate the final portfolio page with the data from the api '

--- client/js/finished_portfolio.js
+++ client/js/finished_portfolio.js
@@ -1,30 +1,43 @@
-console.log('hello');
+const userName = document.querySelector('.user-name');
+const userLocation = document.querySelector('.user-location');
+const userWelcome = document.querySelector('.user-welcome');
+const contactPitch = document.querySelector('.contact-pitch');
+const emailAnchor = document.querySelector('.email-anchor');
+const githubAnchor = document.querySelector('.github-anchor');
+const copyrightName = document.querySelector('.copyright-name');
+const linkedin = document.querySelector('.linkedin');
+const instagram = document.querySelector('.instagram');
+const twitter = document.querySelector('.twitter');
+const projectsGrid = document.querySelector('.projects-grid');
+const skillsGrid = document.querySelector('.skills-grid');
 
-// const userName = document.querySelector('.user-name');
-// const userLocation = document.querySelector('.user-location');
-// const userWelcome = document.querySelector('.user-welcome');
-// const contactPitch = document.querySelector('.contact-pitch');
-// const emailAnchor = document.querySelector('.email-anchor');
-// const githubAnchor = document.querySelector('.github-anchor');
-// const copyrightName = document.querySelector('.copyright-name');
-// const linkedin = document.querySelector('.linkedin');
-// const instagram = document.querySelector('.instagram');
-// const twitter = document.querySelector('.twitter');
-// const projectsGrid = document.querySelector('.projects-grid');
-// const skillsGrid = document.querySelector('.skills-grid');
+axios.get(`/api/portfolios/1`).then(res => {
+    let portfolio = res.data;
 
-// axios.get(`/api/portfolios/1`).then(res => {
-//     let portfolio = res.data;
+    populatePage(portfolio);
 
-//     populatPage(portfolio);
+})
 
-// })
+function populatePage(data) {
+    console.log(data.message);
+    let content = data.results[0]
+    console.log(content);
+    addTextContent(content);
 
-// function populatePage() {
-//     console.log('working');
-    
-// }
+}
 
+const addTextContent =  content => {
+    userName.textContent = content.user_name
+    userLocation.textContent = content.user_location
+    userWelcome.textContent = content.user_welcome
+    contactPitch.textContent = content.contact_pitch
+    emailAnchor.textContent = content.email
+    githubAnchor.textContent = content.github_url 
+    copyrightName.textContent = `- ${content.user_name}`
+    linkedin.href = content.linkedin_url
+    instagram.href = content.instagram_url
+    twitter.href = content.twitter_url
+}
 
 
 // window.addEventListener('load', populatePage)
