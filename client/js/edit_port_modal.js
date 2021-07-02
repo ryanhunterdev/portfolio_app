8d46044c92682f74b5ae0cd1af8009329d1ded60
Sophie Townsend <se.townsend31@gmail.com>
2021-07-02 19:38:16 +1000

    pop up

--- client/js/edit_port_modal.js
+++ client/js/edit_port_modal.js
@@ -10,7 +10,6 @@ const editAboutDiv = document.querySelector('.edit_about_div')
 const editAboutMeBtn = document.querySelector('.edit_aboutMe')
 const editContactBtn = document.querySelector('.edit_contact')
 const editContactDiv = document.querySelector('.edit_contact_div')
-// const editProjectsBtn = document.querySelector('.edit_projects')
 const editProjectsDiv = document.querySelector('.edit_project_div')
 // const editSkillsBtn = document.querySelector('.edit_skills')
 const editSkillsDiv = document.querySelector('.edit_skill_div')
@@ -59,17 +58,23 @@ function editColorFont() {
 
 
 window.onclick = function(event) {
-    if (event.target !== editRightColorBtn && !event.target.closest('.edit_fontsColors_div') && editColorDiv.style.display === "block") {
-      editColorDiv.style.display = "none";
-    } else if (event.target !== editAboutMeBtn && !event.target.closest('.edit_about_div') && editAboutDiv.style.display === "block"){
-        editAboutDiv.style.display = "none";
-    } else if (event.target !== editContactBtn && !event.target.closest('.edit_contact_div') && editContactDiv.style.display === "block"){
-        editContactDiv.style.display = "none";
-    } else if (event.target !== editContactBtn && !event.target.closest('.edit_contact_div') && editContactDiv.style.display === "block"){
-        editContactDiv.style.display = "none";
-    } 
-}
+    const editProjectsBtns = document.querySelectorAll('.edit_projects')
 
+        if (event.target !== editRightColorBtn && !event.target.closest('.edit_fontsColors_div') && editColorDiv.style.display === "block") {
+        editColorDiv.style.display = "none";
+        } else if (event.target !== editAboutMeBtn && !event.target.closest('.edit_about_div') && editAboutDiv.style.display === "block"){
+            editAboutDiv.style.display = "none";
+        } else if (event.target !== editContactBtn && !event.target.closest('.edit_contact_div') && editContactDiv.style.display === "block"){
+            editContactDiv.style.display = "none";
+        } 
+
+        editProjectsBtns.forEach(prjBtn => { 
+            if (event.target !== prjBtn && !event.target.closest('.edit_project_div') && editProjectsDiv.style.display === "block" ){
+                console.log(event.target);
+            } 
+        })
+}
+// && !event.target.closest('.edit_project_div') && editProjectsDiv.style.display === "block"
 
 // listeners for buttons
 projectGridDiv.addEventListener('click', (e)=>{
