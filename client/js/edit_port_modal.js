069eca1590f3247cc86aeac8f510be666eefaf89
Sophie Townsend <se.townsend31@gmail.com>
2021-07-02 17:59:53 +1000

    popups

--- client/js/edit_port_modal.js
+++ client/js/edit_port_modal.js
@@ -2,7 +2,6 @@
 ///////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////////
-console.log('file is connected')
 
 const projectGridDiv = document.querySelector('.projects-grid')
 
@@ -11,9 +10,9 @@ const editAboutDiv = document.querySelector('.edit_about_div')
 const editAboutMeBtn = document.querySelector('.edit_aboutMe')
 const editContactBtn = document.querySelector('.edit_contact')
 const editContactDiv = document.querySelector('.edit_contact_div')
-const editProjectsBtn = document.querySelector('.edit_projects')
+// const editProjectsBtn = document.querySelector('.edit_projects')
 const editProjectsDiv = document.querySelector('.edit_project_div')
-const editSkillsBtn = document.querySelector('.edit_skills')
+// const editSkillsBtn = document.querySelector('.edit_skills')
 const editSkillsDiv = document.querySelector('.edit_skill_div')
 
 // add 
@@ -30,37 +29,31 @@ const editRightColorBtn = document.querySelector('.colorRightBtn')
 const exitBtn = document.querySelector('.exit')
 
 function editAboutMe() {
-    return editAboutDiv.style.display = "block";
+    editAboutDiv.style.display = "block";
 }
 
 function editContact() {
-
-    return editContactDiv.style.display = "block";
+    editContactDiv.style.display = "block";
 }
 
 function editProject() {
-
-    return editProjectsDiv.style.display = "block";
+    editProjectsDiv.style.display = "block";
 }
 
 function editSkills() {
-
-    return editSkillsDiv.style.display = "block";
+    editSkillsDiv.style.display = "block";
 }
 
 function addProject() {
-
-    return addProjectDiv.style.display = "block";
+    addProjectDiv.style.display = "block";
 }
 
 function addSkill() {
-
-    return addSkillsDiv.style.display = "block";
+    addSkillsDiv.style.display = "block";
 }
 
 function editColorFont() {
-
-    return editColorDiv.style.display = 'block';
+    editColorDiv.style.display = 'block';
 }
 
 
@@ -78,18 +71,18 @@ function editColorFont() {
 
 
 // listeners for buttons
-// projectGridDiv.addEventListener('click', (e)=>{
-//     if(e.target.classList.contains('edit_projects')){
-//         editProject()
-//     }
-// })
+projectGridDiv.addEventListener('click', (e)=>{
+    if(e.target.classList.contains('edit_projects')){
+        editProject()
+    }
+})
 editLeftColorBtn.addEventListener('click', editColorFont);
 editRightColorBtn.addEventListener('click', editColorFont);
 editAboutMeBtn.addEventListener('click', editAboutMe);
 editContactBtn.addEventListener('click', editContact);
-addProjectBtn.addEventListener('click', addProject);
-editSkillsBtn.addEventListener('click', editSkills);
-addSkillsBtn.addEventListener('click', addSkill);
+// addProjectBtn.addEventListener('click', addProject);
+// editSkillsBtn.addEventListener('click', editSkills);
+// addSkillsBtn.addEventListener('click', addSkill);
 
 
 // -----------------------------------------------------
