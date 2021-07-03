c5bcd4b1981d1be4648e9fdb0b2fcd122aa61d1d
Ryan Hunter <ryanhunterdev@gmail.com>
2021-07-03 14:31:47 +1000

    rebase

---
+++ client/js/skills.js
@@ -0,0 +1,19 @@
+const addSkillForm = document.querySelector('.add-skill-form');
+
+addSkillForm.addEventListener('submit', event => {
+    event.preventDefault();
+
+    let formData = getFormData(addSkillForm);
+
+    axios
+        .patch(`/api/portfolios/add/skills/${portfolioId}`, formData)
+        .then(res => {
+            addSkillsDiv.style.display = "none";
+            addSkillForm.reset();
+            let newData = res.data
+            console.log(newData);
+            newData.forEach(skill => {
+                skillsGrid.appendChild(createSkill(skill))
+            })
+        })   
+})
