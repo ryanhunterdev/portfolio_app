656f75a65c94b930ec826f1c31e02d45cb77f292
Sophie Townsend <se.townsend31@gmail.com>
2021-07-04 11:44:07 +1000

    style routes

--- controllers/styles-controller.js
+++ controllers/styles-controller.js
@@ -28,4 +28,33 @@ router.patch('/right-background/:id', (req, res) =>{
         })
 })
 
+router.patch('/right-heading-color/:id', (req, res) =>{
+    let user_id = Number(req.params.id)
+    Portfolio
+        .updateStyles(req.body.heading_color_right, "right_heading_color", user_id)
+        .then(dbRes => {
+            res.status(201).json({
+                message: "project was successfully added",
+                allStyles: dbRes.rows[0],
+                headingColorRight: dbRes.rows[0].styles.right_heading_color
+            })
+        })
+})
+
+
+router.patch('/left-heading-color/:id', (req, res) =>{
+    let user_id = Number(req.params.id)
+    Portfolio
+        .updateStyles(req.body.heading_color_left, "left_heading_color", user_id)
+        .then(dbRes => {
+            res.status(201).json({
+                message: "project was successfully added",
+                allStyles: dbRes.rows[0],
+                headingColorLeft: dbRes.rows[0].styles.left_heading_color
+            })
+        })
+})
+
+
+
   module.exports = router
