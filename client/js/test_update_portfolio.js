84ac15a0929d980866b17dbd8b0850d7d64ea5b8
Graham <grahameganwyer@gmail.com>
2021-07-04 15:18:54 +1000

    new template test

--- client/js/test_update_portfolio.js
+++ client/js/test_update_portfolio.js
@@ -1,19 +1,19 @@
-const userName = document.querySelector('.user-name');
-const userLocation = document.querySelector('.user-location');
-const userWelcome = document.querySelector('.user-welcome');
-const contactPitch = document.querySelector('.contact-pitch');
-const emailAnchor = document.querySelector('.email-anchor');
-const githubAnchor = document.querySelector('.github-anchor');
-const copyrightName = document.querySelector('.copyright-name');
-const linkedin = document.querySelector('.linkedin');
-const instagram = document.querySelector('.instagram');
-const twitter = document.querySelector('.twitter');
-const projectsGrid = document.querySelector('.projects-grid');
-const skillsGrid = document.querySelector('.skills-grid');
-const frontEnd = document.querySelector('.front-end')
-const backEnd = document.querySelector('.back-end')
-const other = document.querySelector('.other')
-const url = window.location.pathname;
+// const userName = document.querySelector('.user-name');
+// const userLocation = document.querySelector('.user-location');
+// const userWelcome = document.querySelector('.user-welcome');
+// const contactPitch = document.querySelector('.contact-pitch');
+// const emailAnchor = document.querySelector('.email-anchor');
+// const githubAnchor = document.querySelector('.github-anchor');
+// const copyrightName = document.querySelector('.copyright-name');
+// const linkedin = document.querySelector('.linkedin');
+// const instagram = document.querySelector('.instagram');
+// const twitter = document.querySelector('.twitter');
+// const projectsGrid = document.querySelector('.projects-grid');
+// const skillsGrid = document.querySelector('.skills-grid');
+// const frontEnd = document.querySelector('.front-end')
+// const backEnd = document.querySelector('.back-end')
+// const other = document.querySelector('.other')
+// const url = window.location.pathname;
 
 portfolioId = url.slice(url.indexOf('t/') + 2)
 
